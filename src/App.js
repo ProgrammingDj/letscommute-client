bd3056b37c14f6bc049fbc8e89c452653aa8d866
Eirik Klebo <eiriktobias@gmail.com>
2023-06-29 16:52:23 +0200

    changed imoirt router inn app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Router, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import ExplorePage from "./pages/ExplorePage";
@@ -16,7 +16,6 @@ function App() {
       <Navbar />
 
       <Routes>
-        
         <Route path="/" element={<HomePage />} />
 
         <Route
