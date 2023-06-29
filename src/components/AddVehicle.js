09a21ad12ff0d3f486b66a3fb8b5ececf8672d1a
ProgrammingDj <djamelpettes@gmail.com>
2023-06-29 16:08:58 +0200

    Addvehicle.js completed

--- src/components/AddVehicle.js
+++ src/components/AddVehicle.js
@@ -3,9 +3,12 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5005";
 
-function AddTask(props) {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+function AddVehicle(props) {
+  const [driver, setDriver] = useState("");
+  const [vehicle, setVehicle] = useState("");
+  const [vehicleImage, setVehicleImage] = useState("");
+  const [probationalDriversLicense, setProbationalDriversLicense] = useState("");
+  const [carSharing, setCarSharing] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,51 +17,75 @@ function AddTask(props) {
     const storedToken = localStorage.getItem("authToken");
 
     // We need the project id when creating the new task
-    const { projectId } = props;
+    const { rideId } = props;
     // Create an object representing the body of the POST request
-    const requestBody = { title, description, projectId };
+    const requestBody = { driver, vehicle, vehicleImage, probationalDriversLicense, carSharing, projectId };
 
     axios
-      .post(`${API_URL}/api/tasks`, requestBody, {
+      .post(`${API_URL}/api/vehicle`, requestBody, {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
       .then((response) => {
         // Reset the state to clear the inputs
-        setTitle("");
-        setDescription("");
+        setDriver("");
+        setVehicle("");
+        setVehicleImage("");
+        setProbationalDriversLicense("");
+        setCarSharing("");
 
         // Invoke the callback function coming through the props
-        // from the RideDetailsPage, to refresh the project details
+        // from the RideDetailsPage, to refresh the ride details
         props.refreshProject();
       })
       .catch((error) => console.log(error));
   };
 
   return (
-    <div className="AddTask">
-      <h3>Add New Task</h3>
+    <div className="AddVehicle">
+      <h3>Add New Vehicle</h3>
 
       <form onSubmit={handleSubmit}>
-        <label>Title:</label>
-        <input
-          type="text"
-          name="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-
-        <label>Description:</label>
+      <label>Driver:</label>
         <textarea
-          type="text"
-          name="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          name="Driver"
+          value={driver}
+          onChange={(e) => setDriver(e.target.value)}
         />
 
-        <button type="submit">Add Task</button>
+        <label>Vehicle:</label>
+        <textarea
+          name="vehicle"
+          value={vehicle}
+          onChange={(e) => setVehicle(e.target.value)}
+        />
+
+        <label>VehicleImage:</label>
+        <textarea
+          name="VehicleImage"
+          value={vehicleImage}
+          onChange={(e) => setVehicleImage(e.target.value)}
+        />
+
+        <label>Probational Driver's License:</label>
+        <textarea
+          name="Probational Driver's License"
+          value={probationalDriversLicense}
+          onChange={(e) => setProbationalDriversLicense(e.target.value)}
+        />
+
+        <label>Carsharing:</label>
+        <textarea
+          name="Carsharing"
+          value={carSharing}
+          onChange={(e) => setCarSharing(e.target.value)}
+        />
+
+
+
+        <button type="submit">Add Vehicle</button>
       </form>
     </div>
   );
 }
 
-export default AddTask;
+export default AddVehicle;
