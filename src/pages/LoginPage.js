dc6fe004a238246939a357881b4729d3e31352eb
Eirik Klebo <eiriktobias@gmail.com>
2023-06-29 11:02:27 +0200

    updated signup page

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,109 @@
+import { useState, useContext } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+
+const API_URL = "http://localhost:5005";
+
+function LoginPage(props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [probationaryDriver, setProbationaryDriver] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const { storeToken, authenticateUser } = useContext(AuthContext);
+
+  const handleName = (e) => setName(e.target.value);
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleProfileImage = (e) => setProfileImage(e.target.value);
+  const handlePhoneNumber = (e) => setPhoneNumber(e.target.value);
+  const handleProbationaryDriver = (e) => setProbationaryDriver(e.target.value);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = {
+      name,
+      email,
+      password,
+      profileImage,
+      phoneNumber,
+      probationaryDriver,
+    };
+
+    axios
+      .post(`${API_URL}/auth/login`, requestBody)
+      .then((response) => {
+        // Request to the server's endpoint `/auth/login` returns a response
+        // with the JWT string ->  response.data.authToken
+        console.log("JWT token", response.data.authToken);
+
+        // Save the token in the localStorage.
+        storeToken(response.data.authToken);
+
+        // Verify the token by sending a request
+        // to the server's JWT validation endpoint.
+        authenticateUser(); // <== ADD
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
+  return (
+    <div className="LoginPage">
+      <h1>Login</h1>
+
+      <form onSubmit={handleLoginSubmit}>
+        <label>Name:</label>
+        <input type="text" name="name" value={name} onChange={handleName} />
+        <label>Email:</label>
+        <input type="email" name="email" value={email} onChange={handleEmail} />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePassword}
+        />
+        <label>Profile image:</label>
+        <input
+          type="img"
+          name="profileImage"
+          value={profileImage}
+          onChange={handleProfileImage}
+        />
+        <label>Phone number:</label>
+        <input
+          type="number"
+          name="phoneNumber"
+          value={phoneNumber}
+          onChange={handlePhoneNumber}
+        />
+        <label>Probationary driver:</label>
+        <input
+          type="text"
+          name="probationaryDriver"
+          value={probationaryDriver}
+          onChange={handleProbationaryDriver}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      <p>Don't have an account yet?</p>
+      <Link to={"/signup"}> Sign Up</Link>
+    </div>
+  );
+}
+
+export default LoginPage;
