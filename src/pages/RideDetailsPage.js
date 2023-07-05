01cbf7f4a0e432f2e15beca89f51a53f07d4d472
ProgrammingDj <djamelpettes@gmail.com>
2023-07-05 18:48:33 +0200

    added labels to RideDetailsPage

--- src/pages/RideDetailsPage.js
+++ src/pages/RideDetailsPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import AddVehicle from "../components/AddVehicle";
@@ -7,51 +7,56 @@ import VehicleCard from "../components/VehicleCard";
 const API_URL = "http://localhost:5005";
 
 function RideDetailsPage(props) {
-  const [ride, setRide] = useState(null);
-  const { rideId } = useParams();
+const [ride, setRide] = useState(null);
+const { rideId } = useParams();
 
-  const getRide = () => {
-    // Get the token from the localStorage
-    const storedToken = localStorage.getItem("authToken");
-    console.log(rideId);
-    // Send the token through the request "Authorization" Headers
-    axios
-      .get(`${process.env.REACT_APP_SERVER_URL}/api/rides/${rideId}`, {
-        headers: { Authorization: `Bearer ${storedToken}` },
-      })
+const getRide = () => {
+// Get the token from the localStorage
+const storedToken = localStorage.getItem("authToken");
 
-      .then((response) => {
-        const oneRide = response.data;
-        setRide(oneRide);
-      })
-      .catch((error) => console.log(error));
-  };
+axios
+  .get(`${API_URL}/api/rides/${rideId}`, {
+    headers: { Authorization: `Bearer ${storedToken}` },
+  })
+  .then((response) => {
+    const oneRide = response.data;
+    setRide(oneRide);
+  })
+  .catch((error) => console.log(error));
 
-  useEffect(() => {
-    getRide();
-  }, []);
+};
 
-  return (
-    <div className="RideDetails">
-      {ride && (
-        <>
-          <h1 className="">
-            <span>{ride.fromCity}</span> → <span>{ride.toCity}</span>
-          </h1>
-          <p>{ride.intervalOfRides}</p>
-          <p>{ride.seats}</p>
-          <p>{ride.vehicle.vehicle}</p>
-        </>
-      )}
+useEffect(() => {
+getRide();
+}, []);
 
-      <Link to="/rides">
-        <button>Back to rides</button>
-      </Link>
-      <Link to={`/rides/edit/${rideId}`}>
-        <button>Edit Ride</button>
-      </Link>
-    </div>
-  );
+return (
+<div className="RideDetails">
+{ride && (
+<>
+<h1 className="">
+<span>{ride.fromCity}</span> → <span>{ride.toCity}</span>
+</h1>
+<p>Interval of rides: {ride.intervalOfRides}</p>
+<p>Seats available: {ride.seats}</p>
+<p>Vehicle: {ride.vehicle}</p>
+<p>Driver name: {ride.driver}</p>
+<p>Probational Driver: {ride.probationalDriversLicense}</p>
+{ride.vehicles.map((vehicle) => (
+<VehicleCard key={vehicle._id} {...vehicle} />
+))}
+</>
+)}
+
+  <Link to="/rides">
+    <button>Back to rides</button>
+  </Link>
+
+  <Link to={`/rides/edit/${rideId}`}>
+    <button>Edit Ride</button>
+  </Link>
+</div>
+    );
 }
 
 export default RideDetailsPage;
