2eb9507652212b675d1d81ab2d3d3034cde98a84
ProgrammingDj <djamelpettes@gmail.com>
2023-07-06 16:01:21 +0200

    added MyRidesPage and got it working

--- src/pages/MyRidesPage.js
+++ src/pages/MyRidesPage.js
@@ -1,38 +1,43 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import RideCard from "../components/RideCard";
+import { AuthContext } from "../context/auth.context";
 
 const API_URL = "http://localhost:5005";
 
-function ListRides() {
+function MyRidesPage() {
   const [rides, setRides] = useState([]);
+  const { user } = useContext(AuthContext);
 
-  const getAllRides = () => {
-    // Get the token from the localStorage
+  const getUserRides = () => {
     const storedToken = localStorage.getItem("authToken");
 
-    // Send the token through the request "Authorization" Headers
     axios
-      .get(`${process.env.REACT_APP_SERVER_URL}/api/rides/myrides`, {
+      .get(`${process.env.REACT_APP_SERVER_URL}/api/rides`, {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
-      .then((response) => setRides(response.data))
+      .then((response) => {
+        const filteredRides = response.data.filter(
+          (ride) => ride.vehicle.owner === user?._id
+        );
+        setRides(filteredRides)
+      })
       .catch((error) => console.log(error));
   };
 
-  // We set this effect will run only once, after the initial render
-  // by setting the empty dependency array - []
   useEffect(() => {
-    getAllRides();
-  }, []);
+    getUserRides();
+  }, [user]);
 
   return (
-    <div className="ListRides">
-      {rides.map((ride) => (
-        <RideCard key={ride._id} {...ride} />
-      ))}
+    <div className="listContainer">
+      <div className="listmyRides">
+        {rides.map((ride) => (
+          <RideCard key={ride._id} {...ride} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default ListRides;
+export default MyRidesPage;
